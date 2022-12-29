3c980970ae8564c24175cb803882b5ea744729f8
Valeri Vinnikov <valeri.vinnikov@gmail.com>
2022-12-29 12:40:41 +0100

    Final3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!y')
+    res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
 
@@ -15,6 +15,8 @@ app.listen(process.env.PORT || 3000)
 const MONGODB_URI = 'mongodb+srv://dbuser:$Rainbow1@cluster0.ooeal.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 mongoose.set('strictQuery', false);
+
+//Connect to MongoDb
 mongoose.connect(MONGODB_URI).then(()=>
 {
     console.log('MongoDB is connected!!!!')
@@ -41,20 +43,25 @@ ClientsPostSchema = new ClientsSchema({
 // Define Clients model
 ClientsPost = mongoose.model('ClientsPost', ClientsPostSchema, 'clients');
 
-// Web service find document by caller
+// Web service find caller data by calling number
 app.post('/api/findClientByTelNumber', (req, res) => {
     console.log("/api/findClientByTelNumber");
-    console.log("Find client by calling number");
-    console.log(req.body);
+    console.log("Find client data by calling number");
     var callingNumber = req.body.callingNumber;
     console.log("callingNumber = " + callingNumber);
     ClientsPost.findOne({ phoneNumber: callingNumber })
         .then((data) => {
-            console.log("First name: " + data.firstName);
-            console.log("Last name: " + data.lastName);
-            console.log("Email: " + data.email);
+            if (data != null)
+            {
+                console.log("First name: " + data.firstName);
+                console.log("Last name: " + data.lastName);
+                console.log("Email: " + data.email);
+            }
+            else
+            {
+                console.log("Not found");
+            }            
             res.json(data);
-
         })
         .catch((error) => {
             console.log('error', error);
